f853760cfe138246f7733f561ff983ef6c5fa577
João Victor Ribeiro Alvarenga <joaooficial0411@gmail.com>
2025-07-15 19:45:42 -0300

    update

--- Exercicio State/src/app/page.tsx
+++ Exercicio State/src/app/page.tsx
@@ -20,7 +20,7 @@ const Page = () => {
     <>
       {!showImage &&
         <section className='flex flex-col w-full h-screen items-center justify-start'>
-            <h1 className='text-3xl m-4'>Fotos Intergalaticas</h1>
+            <h1 className='text-3xl m-4'>Fotos Intergalácticas</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-4/5 h-full'>
               {listPhotos.map(item => 
                 <div onClick={() => imgSelected(item.id)}>
