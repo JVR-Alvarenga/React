8f2b4cc82af7e24b349385a36d3548288f72e65c
João Victor Ribeiro Alvarenga <joaooficial0411@gmail.com>
2025-07-16 19:34:52 -0300

    update closeImage

--- Exercicio State/src/app/components/ItemPhoto.tsx
+++ Exercicio State/src/app/components/ItemPhoto.tsx
@@ -10,7 +10,7 @@ export const ItemPhoto = ({ imgUrl, closeImage }: Props) => {
     return (
         <section className="w-full h-screen flex items-start justify-center">
             <img className="w-4/5 h-screen object-contain" src={imgUrl} />
-            <div className="w-20 bold text-center text-3xl object-contain cursor-pointer" onClick={closeImage}> x </div>
+            <div className="w-1/10 bold text-center h-screen text-3xl object-contain cursor-pointer" onClick={closeImage}> x </div>
         </section>
     );
 }
