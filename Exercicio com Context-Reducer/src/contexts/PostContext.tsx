f1941b4c8173bae37ae68288180b26e9b19e038b
João Victor Ribeiro Alvarenga <joaooficial0411@gmail.com>
2025-07-30 06:56:13 -0300

    using localStorage

--- Exercicio com Context-Reducer/src/contexts/PostContext.tsx
+++ Exercicio com Context-Reducer/src/contexts/PostContext.tsx
@@ -1,15 +1,23 @@
 "use client"
 
 import { PostContextType } from "@/types/PostContextType";
-import { createContext, ReactNode, useContext, useReducer } from "react";
+import { createContext, ReactNode, useContext, useEffect, useReducer } from "react";
 import { postReducer } from "@/reducers/postReducer";
+
+const STORAGE_KEY = 'postContextContent';
 
 export const PostContext = createContext<PostContextType | null>(null);
 
 export const usePosts = () => useContext(PostContext);
 
 export const PostProvider = ({ children }: {children: ReactNode}) => {
-    const [posts, dispatch] = useReducer(postReducer, []);
+    const [posts, dispatch] = useReducer(postReducer, 
+        JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+    );
+
+    useEffect(() => {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(posts));
+    }, [posts]);
 
     return (
         <PostContext.Provider value={{ posts, dispatch }}>
